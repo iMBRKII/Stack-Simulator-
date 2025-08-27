a67907c115107164979a41c8dd3af1139471598a
0xMk <120375991+iMBRKII@users.noreply.github.com>
2025-08-27 20:37:57 +0300

    Initial Gradle project setup

---
+++ app/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import gui.Frame;
+
+/**
+ *
+ * @author MK
+ */
+public class Main extends Frame{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
